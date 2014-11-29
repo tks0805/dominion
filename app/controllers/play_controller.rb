48ff728cc30d44664f0bcbbb98a3d34ce0417d23
tks0805 <tks@takashi-no-MacBook-Air.local>
2014-11-29 17:43:06 +0900

    １７４２
    １７４２

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,10 +1,30 @@
 ﻿class PlayController < ApplicationController
 
-def index
-  if params[:list][:name].present?
-    @card = Card.where(:id => params[:list][:name]).first
+  # (1)
+  include ActionController::Live
+ 
+  def stream
+    # (2)
+    response.headers['Content-Type'] = 'text/event-stream'
+ 
+    10.times do |i|
+      # (3)
+      response.stream.write("event: message\n")
+      response.stream.write("data: Hello #{i+1}\n\n")
+      sleep 1
+    end
+ 
+    response.stream.write("event: done\n")
+    response.stream.write("data: done\n\n")
+  ensure
+    # (4)
+    response.stream.close
   end
 
-end
+  def index
+    if params[:list][:name].present?
+      @card = Card.where(:id => params[:list][:name]).first
+    end
+  end
 
 end
